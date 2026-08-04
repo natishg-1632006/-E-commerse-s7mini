af71dd20543bb561f6acaeffbe07c13a051896e4
natishg-1632006 <natishg8@gmail.com>
2026-08-04 15:26:44 +0530

    fix: correct comment for Lambda creation in lambda.js

--- e-com_backend/coupon-service/lambda.js
+++ e-com_backend/coupon-service/lambda.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const serverless = require("serverless-http");
 const app = require("./src/app");
-// Create 
+// Create Lambda
 module.exports.handler = serverless(app, {
   request(request, event, context) {
     request.event = event;
