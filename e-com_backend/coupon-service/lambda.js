424a4b6823cfa4e2ca3abda5012d9f8d576d07d2
natishg-1632006 <natishg8@gmail.com>
2026-08-04 14:38:55 +0530

    fix: remove unnecessary blank lines in lambda.js

--- e-com_backend/coupon-service/lambda.js
+++ e-com_backend/coupon-service/lambda.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
-
 const serverless = require("serverless-http");
 const app = require("./src/app");
-
 // Create Lambda handler
 module.exports.handler = serverless(app, {
   request(request, event, context) {
